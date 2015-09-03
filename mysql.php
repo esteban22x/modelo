48cb0c64fde049d19e06d7abef9767cf7a021c96
Esteban <esteban22x@gmail.com>
2015-09-03 17:04:45 -0500

    Documentando codigo
    Agregue comentarios descriptivos de cada funcion y arregle el
    mysql_close()

--- mysql.php
+++ mysql.php
@@ -4,17 +4,17 @@
 //Version 1.1 con MySQLi modificada por Esteban Delgado
 class mysql {
     
-    var $servidor = "";
-    var $usuario = "";
-    var $clave = "";
-    var $base = "";
-    var $conexion;
+    var $servidor = "";var $usuario = "";var $clave = "";var $base = "";var $conexion;
     var $bandera = false;
     function __construct(){
         $this->servidor = "localhost";
         $this->usuario = "root";
         $this->clave = "";
     }
+    /*
+     Conectar: Con los datos suministrador en el archivo conectar.php establece una conexion
+     a la base de datos
+    */
     function conectar()
 	{
             $this->conexion = new mysqli($this->servidor,$this->usuario,$this->clave,$this->base) 
@@ -23,34 +23,56 @@ class mysql {
             $this->conexion->set_charset("utf8");
             return $this->conexion;
     }
-    
+    /*
+     Cerrar: Cierra la conexión previamente abierta
+     Uso $conexion->cerrar();
+    */
     function cerrar()
 	{
         if($this->bandera)
 		{
-            mysqli::close();
+            $this->conexion->close();
         }
     }
-    
+    /*
+     Consultar: Hace una consulta (query que tambien puede ser de modificacion,eliminacion o inserción) a la base de datos
+     Uso: $consulta = $conexion->consultar("SELECT * FROM tabla");
+    */
     function consultar($sql)
 	{
         return $this->conexion->query($sql);
     }
+    /*
+     f_fila:Mostrara el resultado de una consulta en un array asociativo
+     representado por el objeto de una fila especificada 
+     Uso: $conexion->f_fila($consulta)->nombre;
+    */
 
     function f_fila($sql)
 	{
         return $sql->fetch_object();
     }
+    /*
+     f_arreglo: Mostrara el resultado de una consulta en un array asociativo
+     donde el nombre de cada columna es representado por la clave dentro del array
+    */
 
     function f_arreglo($sql)
 	{
         return $sql->fetch_assoc();
     }
-
+    /*
+     f_total: Muestra el numero de filas resultantes en la consulta
+     Uso: $conexion->f_total($consulta);
+    */
     function f_total($sql)
 	{
         return $sql->num_rows;
     }  
+    /*
+     liberar_consulta() cierra la conexion de una consulta especificada
+     Uso : $conexion->liberar_consulta($consulta);
+    */
     
     function liberar_consulta($sql)
 	{
